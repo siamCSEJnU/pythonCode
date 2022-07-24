a8443a193a3dfd1f13bf8cf89eb8b113755b99ca
siamCSEjnu <99203101+siamCSEJnU@users.noreply.github.com>
2022-07-25 00:33:10 +0600

    Update squareTurtrle.py

--- squareTurtrle.py
+++ squareTurtrle.py
@@ -1,14 +1,11 @@
 import turtle
 
-turtle.shape("turtle")
+turtle.shape("turtle")      
 
-turtle.speed(1)
-turtle.forward(100)
-turtle.left(90)
-turtle.forward(100)
-turtle.left(90)
-turtle.forward(100)
-turtle.left(90)
-turtle.forward(100)
-turtle.left(90)
+turtle.speed(10)  
+for  i in range(4) :        #defult =0(most speed),1 to 10(increasing the speed)
+    turtle.forward(100)
+    turtle.left(90)
+
+
 turtle.exitonclick()
