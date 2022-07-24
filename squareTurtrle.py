6291b285c5a096f0ead873010a6eaccf370fad11
siamCSEjnu <99203101+siamCSEJnU@users.noreply.github.com>
2022-07-25 00:25:52 +0600

    Add files via upload

---
+++ squareTurtrle.py
@@ -0,0 +1,14 @@
+import turtle
+
+turtle.shape("turtle")
+
+turtle.speed(1)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.exitonclick()
